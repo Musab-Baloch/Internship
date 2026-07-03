6b9eb23902f9b821ebdc597b2a1c23f0d572da3f
Musab Baloch <science1017270@gmail.com>
2026-07-04 01:14:16 +0500

    Renamed test file

---
+++ ..PHASE_1/testapi.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 28 19:11:47 2026
+
+@author: Musab Baloch
+"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# GET
+# POST
+# PUT
+# DELETE
+
+@app.get("/delete_account")
+def delete_acct():
+    pass
+
+@app.get("/create-account")
+def create_acct():
+    return {"name" : "Hermano"}
+
+
--- ..PHASE_1/untitled0.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 28 19:11:47 2026
-
-@author: Musab Baloch
-"""
-
